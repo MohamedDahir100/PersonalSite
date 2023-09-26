7fe96270a0ffbb547a4f5e6a237ca423ef4e6def
Mohamed Dahir <132926985+MohamedDahir100@users.noreply.github.com>
2023-09-26 12:17:16 -0400

    Added more to pages, removed build

--- pages.js
+++ pages.js
@@ -1,12 +1,19 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/MohamedDahir100/Portfolio.git"; // Update this URL
+const repoURL = "https://github.com/MohamedDahir100/Portfolio.git";
+const token = ""; // Replace with your GitHub token
 
 ghpages.publish(
   pathname,
   {
     branch: "master",
     repo: repoURL,
+    user: {
+      name: "Mohamed Dahir",
+      email: "mohamed.dahir561@gmail.com",
+    },
+
+    token: token, // Provide your GitHub token here
   },
   (err) => {
     if (err) console.log("ERROR: ", err);
